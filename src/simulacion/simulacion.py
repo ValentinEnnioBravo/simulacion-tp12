ae67f7471a95f0afa06a76c434332e7de1935f7b
Valentin Ennio Bravo <vbravo@frba.utn.edu.ar>
2025-12-01 19:37:29 -0300

    .

--- src/simulacion/simulacion.py
+++ src/simulacion/simulacion.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from fitter import Fitter
 import os
-
+"""
+Ejemplo de uso: AD()
+"""
 # Autodescarga
 AD_dist = stats.gennorm.rvs(beta=39.87762517430417, loc=0.02008661643245054, scale=0.010103831003727536, size=4000)
 def AD(): return np.random.choice(AD_dist)
 
-# Costo Falla
+# Costo Falla - REVISAR
 CF_dist = stats.pearson3.rvs(skew=1.2672861956124433, loc=7955.175469640658, scale=3763.2127890221027, size=4000)
 def CF(): return np.random.choice(CF_dist)
 
@@ -23,11 +25,11 @@ def DI(): return np.random.choice(DI_dist)
 
 # Generacion diaria CC1
 GD1_dist = stats.gennorm.rvs(beta=3.0613478036342627, loc=719.3923999911451, scale=127.21163991375852, size=4000)
-def GD1(): return np.random.choice(GD1_dist)
+def GDCC1(): return np.random.choice(GD1_dist)
 
 # Generacion diaria CC2
 GD2_dist = stats.gennorm.rvs(beta=4.9423185984406155, loc=610.589328055618, scale=124.80973709648973, size=4000)
-def GD2(): return np.random.choice(GD2_dist)
+def GDCC2(): return np.random.choice(GD2_dist)
 
 # Generacion diaria TV
 GDTV_dist = stats.gennorm.rvs(beta=5.703506370721026, loc=550.2687715652102, scale=124.52965269900714, size=4000)
@@ -36,12 +38,3 @@ def GDTV(): return np.random.choice(GDTV_dist)
 # Potencia Perdida
 PP_dist = stats.tukeylambda.rvs(lam=1.0018233713540219, loc=5.499999999999986, scale=2.5045584283850713, size=4000)
 def PP(): return np.random.choice(PP_dist)
-
-print(AD())
-print(CF())
-print(DV())
-print(DI())
-print(GD1())
-print(GD2())
-print(GDTV())
-print(PP())
